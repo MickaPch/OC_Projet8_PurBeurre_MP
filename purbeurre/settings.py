cda4469e86ada93224bec2f260b05f146b2b6aa4
MickaP <mickael.perchet@gmail.com>
2021-05-02 19:54:09 +0200

    csrf ajax

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import environ
 import django_heroku
-# import dj_database_url
+import dj_database_url
 
 from pathlib import Path
 import os
